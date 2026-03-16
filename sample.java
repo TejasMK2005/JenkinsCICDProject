e8764299982d7935930ae6a84289acdb2ba2dd58
TejasMK2005 <vvce23ise0027@vvce.ac.in>
2026-03-16 17:14:18 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 class sample{
   public static void main(String[] args){
-    System.out.println("hello from jenkins ci/cd pipeline");
+    System.out.println("build triggered using poll scm");
   }
 }
